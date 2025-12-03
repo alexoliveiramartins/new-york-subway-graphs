c9150baf47257e2c9e2119446e02370c06cc6724
marcolasdev <146884614+marcolasdev@users.noreply.github.com>
2025-12-03 17:38:25 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 import pandas as pd
-from graph import build_graph
+from graph import build_graph_range, build_graph_service
 from utils import plot_folium
-MAXRANGEKM = 0.7  # ligações de proximidade (ajuste se necessário)
+MAXRANGEKM = 1.2  # ligações de proximidade (ajuste se necessário)
 CSV = "dados/MTA_Subway_Stations.csv"
 
 
 df = pd.read_csv(CSV)
 # filtrar linhas sem coords
 df = df.dropna(subset=["GTFS Latitude", "GTFS Longitude"])
-G = build_graph(df, MAXRANGEKM)
-plot_folium(G)
+G = build_graph_range(df, MAXRANGEKM)
+plot_folium(G, "tests/subway_graph_range.html")
+print("Número de estações (nós):", G.number_of_nodes())
+print("Número de conexões (arestas):", G.number_of_edges())
 
+F = build_graph_service(df)
+plot_folium(F, "tests/subway_graph_service.html")
+print("Número de estações (nós):", F.number_of_nodes())
+print("Número de conexões (arestas):", F.number_of_edges())
